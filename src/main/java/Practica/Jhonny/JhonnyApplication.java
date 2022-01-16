f18b3be7de98e0107e2b5ed6e02e3d692047ba96
JhonnyDark10 <flores_dark10@outlook.es>
2022-01-16 14:22:28 -0500

    archivos

---
+++ src/main/java/Practica/Jhonny/JhonnyApplication.java
@@ -0,0 +1,13 @@
+package Practica.Jhonny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhonnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Practica/Jhonny/JhonnyApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica.Jhonny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
